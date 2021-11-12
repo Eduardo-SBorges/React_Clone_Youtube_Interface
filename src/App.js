e7f0cc277669e4ed65ce2b80bde88f90b544544c
Eduardo Borges <82469192+Eduardo-SBorges@users.noreply.github.com>
2021-11-12 17:24:57 -0300

    add: topbar and images to use in this project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import Home from './Home';
 
 const App = () => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3f51b5',
+      },
+    },
+  });
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 };
 
