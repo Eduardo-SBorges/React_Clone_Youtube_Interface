c806a98c2bceaaa0c5c941e38c8595a2fe28cf8b
Eduardo Borges <82469192+Eduardo-SBorges@users.noreply.github.com>
2021-11-13 16:50:04 -0300

    add: videos recommends and dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
 import HomePage from './HomePage';
 
+const useStyles = makeStyles({
+  root: {},
+});
+
 const App = () => {
+  const [darkMode, setDarkMode] = React.useState(false);
+
   const theme = createMuiTheme({
+    spacing: 4,
     palette: {
+      type: darkMode ? 'dark' : 'light',
       primary: {
         main: '#f44336',
       },
       secondary: {
-        main: '#3f51b5',
+        main: '#3EA6FF',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
       },
     },
   });
 
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={theme}>
-      <HomePage />
+      <HomePage darkMode={darkMode} setDarkMode={setDarkMode} />
     </ThemeProvider>
   );
 };
